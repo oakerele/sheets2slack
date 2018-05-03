120316c5eb248dfab6bf7c0b22faf7b8cd66cfb5
oakerele <otakerele@gmail.com>
2018-05-03 05:39:12 -0400

    nifty fix

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const sheets = require('google-spreadsheet');
+const async = require('async');
 
 // The creds are generated under the service accounts of a Google Cloud Project
 // After enabling the Google Drive API under APIs and Services
@@ -8,31 +9,58 @@ const sheets = require('google-spreadsheet');
 const credentials = require('./config/service_account.json');
 
 // Replace the id in here with the ID from the Google Sheets URL
-var googleSheet = new sheets('1bJuthZOtLK_pyt0ClEl3K9lbByRlm_LrlbjqnPYvYQU')
+var googleSheets = new sheets('1bJuthZOtLK_pyt0ClEl3K9lbByRlm_LrlbjqnPYvYQU');
 
+/**
+ * NOTE: This is not good code. The code has to accomodate for the limitations of the
+ * google API and also bad human habit of leaving empty rows in sheet instead of a border 😫
+ */
 exports.sheetsToSlack = functions.https.onRequest((request, response) => {
-    // Authenticate with the Google Spreadsheets API.
     const stockNo = request.body.text;
-    
-});
-// var options = {
-//     query: `stocknumber=${stockNo}`
-// }
-var options = {
-    orderby: 'stocknumber',
-    query: `stocknumber=T29219`
-}
-googleSheet.useServiceAccountAuth(credentials, function (err) {
-    if (err) {
-        console.error(err);
-        response.send("Could not authenticate user. See Admin for more details.");
-    }
-    // Get all of the rows from the spreadsheet.
-    googleSheet.getRows(1, options, function (err, rows) {
-        if (err) {
-            console.error(err);
-            response.send("Could not retrieve information. See Admin for more details.");
+    var worksheet;
+
+    // This is a good tool to run each function only after the predecessor is done
+    async.series([
+        function setAuth(step) {
+            // Authenticate the user with creds and pass the callback function
+            googleSheets.useServiceAccountAuth(credentials, step);
+        },
+        function getWorksheet(step) {
+            googleSheets.getInfo(function (err, info) {
+                // Set the worksheet to the first worksheet, 0 indexed
+                worksheet = info.worksheets[0];
+                step();
+            });
+        },
+        function workingWithCells(step) {
+            // Get the stock number column by setting the min and max to it's 1 based index
+            // Scan all the rows since space is a requirement in the google sheet and we can never
+            // be too sure we are at the end of the document
+            worksheet.getCells({
+                'min-row': 1,
+                'max-row': worksheet.rowCount,
+                'min-col': 4,
+                'max-col': 4,
+                'return-empty': true
+            }, function (err, cells) {
+                var row, hasStockNumber = false;
+                for (var index in cells) {
+                    // If the value of each cell is the sane as the stockNo, ding ding we found a winnder
+                    if (cells[index].value == stockNo) {
+                        hasStockNumber = true;
+                        row = cells[index].row;
+                        break;
+                    }
+                }
+                // Respond with an output.
+                if (hasStockNumber) {
+                    response.send("The vehicle stock exists on row " + row);
+                } else {
+                    response.send("Stock does not exist.");
+                }
+            });
         }
-        console.log(rows);
+    ], function(err) {
+        response.send("Something went wrong, make sure you entered the right stock No.");
     });
 });
